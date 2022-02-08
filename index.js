dfb4dc1590c9210ba296189b5f227300fa8bcfd3
Ege Yardımcı <33810618+egeyardimci@users.noreply.github.com>
2022-02-08 19:01:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -23,44 +23,44 @@ function createNewCardHTML(imgSrc,agentName,agentUsage,agentSkills,agentRole,age
     let newCardDescription = document.createElement("p");
     let textDes = document.createTextNode(agentUsage);
     newCardDescription.appendChild(textDes);
+
+    let frontSide = document.createElement("div");
+    frontSide.className = "frontSide";
 	
-	let frontSide = document.createElement("div");
-	frontSide.className = "frontSide";
+    let backSide = document.createElement("div");
+    backSide.className = "backSide";
+    backSide.style= "display : none;";
 	
-	let backSide = document.createElement("div");
-	backSide.className = "backSide";
-	backSide.style= "display : none;";
-	
-	let newAbilityContainer = document.createElement("div");
+    let newAbilityContainer = document.createElement("div");
     newAbilityContainer.className = "abilityContainer";
+
+    for(let i = 0; i < 4; i++){
+    	let newAbilityImage = document.createElement("img");
+    	newAbilityImage.src = agentSkills[i].displayIcon;
+    	newAbilityImage.className = "abilityImage";
+    	newAbilityContainer.appendChild(newAbilityImage);
+    }
 	
-	for(let i = 0; i < 4; i++){
-		let newAbilityImage = document.createElement("img");
-		newAbilityImage.src = agentSkills[i].displayIcon;
-		newAbilityImage.className = "abilityImage";
-		newAbilityContainer.appendChild(newAbilityImage);
-	}
-	
-	let roleDiv = document.createElement("div");
-	roleDiv.className = "agentRoleDiv";
-	
-	let agentRoleImage = document.createElement("img");
-	agentRoleImage.src = agentRoleIMG;
+    let roleDiv = document.createElement("div");
+    roleDiv.className = "agentRoleDiv";
+
+    let agentRoleImage = document.createElement("img");
+    agentRoleImage.src = agentRoleIMG;
     agentRoleImage.className = "agentRoleImage";
 	
-	let agentRoleElement = document.createElement("p");
-	let agentRoleText = document.createTextNode(agentRole);
-	agentRoleElement.className = "agentRoleElement";
-	agentRoleElement.appendChild(agentRoleText);
+    let agentRoleElement = document.createElement("p");
+    let agentRoleText = document.createTextNode(agentRole);
+    agentRoleElement.className = "agentRoleElement";
+    agentRoleElement.appendChild(agentRoleText);
 
-	roleDiv.appendChild(agentRoleElement);
-	roleDiv.appendChild(agentRoleImage);
-	backSide.appendChild(roleDiv);
-	backSide.appendChild(newAbilityContainer);
+    roleDiv.appendChild(agentRoleElement);
+    roleDiv.appendChild(agentRoleImage);
+    backSide.appendChild(roleDiv);
+    backSide.appendChild(newAbilityContainer);
     frontSide.appendChild(newCardImage);
     frontSide.appendChild(newCardText);
     frontSide.appendChild(newCardDescription);
-	newCard.appendChild(frontSide);
+    newCard.appendChild(frontSide);
     newCard.appendChild(backSide);
 
 
@@ -76,13 +76,3 @@ function createNewCard(apiData){
         }
     });
 }
-
-$(document).ready(function(){
-    $(".card").hover(function(){
-      $(this).children(".frontSide").hide();
-      $(this).children(".backSide").show();
-    },function(){
-      $(this).children(".frontSide").show();
-      $(this).children(".backSide").hide();
-    });
-  });
